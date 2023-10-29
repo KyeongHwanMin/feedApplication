8f8ffa56692a6e03a399636b0b8ddc7b80e1ce98
KyeongHwan Min <h45652@naver.com>
2023-10-29 22:08:42 +0900

    FEAT : 통계 앱 생성 및 연결

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/user/", include("accounts.urls")),
     path("api/v1/post/", include("post.urls")),
+    path("api/v1/analytics/", include("analytics.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
